bbab038ddad9dee6f5606d4095194f32fd9bfb04
Emil Uzelac <emil@uzelac.me>
2023-06-11 18:48:32 -0500

    Styles Update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" data-bs-theme="auto">
       <Head />
-      <body className="bg-body-tertiary">
+      <body>
         <Main />
         <NextScript />
       </body>
