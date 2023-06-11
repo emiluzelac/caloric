db59a5620926961bb199656074cf45a6ca0a75a8
Emil Uzelac <emil@uzelac.me>
2023-06-11 13:50:31 -0500

    Header.tsx
    Icon swap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,23 +8,12 @@ const Header: React.FC = () => {
           href="/"
           className="d-flex align-items-center text-dark text-decoration-none"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="me-2"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            strokeWidth="1"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" className="me-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M10 12h2a2 2 0 1 0 0 -4h-2v8"></path>
-            <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z"></path>
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+            <path d="M14 10a2 2 0 1 0 -4 0v4a2 2 0 1 0 4 0"></path>
           </svg>
-          <span>Pavlidou Caloric Calculator</span>
+          <span>Calorie Calculator</span>
         </Link>
       </h1>
       <a
