d58e47f0512398b3b4c3b1aa14374896621a2ba5
Emil Uzelac <emil@uzelac.me>
2023-06-10 23:48:05 -0500

    Update Calculator.tsx

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Calculator: React.FC = () => {
+const Calculator = () => {
   const [sex, setSex] = useState('');
   const [age, setAge] = useState('');
   const [heightFt, setHeightFt] = useState('');
@@ -48,11 +48,11 @@ const Calculator: React.FC = () => {
     setFormSubmitted(true);
   };
 
-  const isInvalid = (value: string) => {
+  const isInvalid = (value) => {
     return formSubmitted && (!value || value.trim() === '');
   };
 
-  const getActivityLevelName = (value: string) => {
+  const getActivityLevelName = (value) => {
     switch (value) {
       case '1.2':
         return 'sedentary';
