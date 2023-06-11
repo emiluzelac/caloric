5a5f0dfbfaa686b761baf94cae6208274f562ead
Emil Uzelac <emil@uzelac.me>
2023-06-11 18:33:12 -0500

    Custom styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ const Footer: React.FC = () => {
   return (
     <footer>
       <hr className="mt-5 mb-4" />
-      <p className="text-muted">
+      <p className=" text-center text-md">
         Created and opened sourced by{' '}
-        <a href="https://github.com/emiluzelac" target="_blank" rel="noopener">
+        <a href="https://github.com/emiluzelac" className="text text-info" target="_blank" rel="noopener">
           Emil Uzelac
         </a>
       </p>
