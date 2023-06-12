747cce25fad468a06ee24c1b93ae965cd6923a12
Emil Uzelac <emil@uzelac.me>
2023-06-11 22:15:16 -0500

    Footer classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <hr className="mt-5 mb-4" />
-      <span className=" text-center text-md">
+      <span className="text-center text-md me-2">
         Created and opened sourced by{' '}
         <a href="https://github.com/emiluzelac" className="text text-info" target="_blank" rel="noopener">
           Emil Uzelac
         </a>
       </span>
-      <span className="me-1 badge bg-info">v1.0.0</span>
+      <span className="badge bg-info">v1.0.0</span>
     </footer>
   );
 };
